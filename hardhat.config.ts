f049075bdb6a847919798a5010a0f57440cdfe1e
Xander Q <xanderqiang@outlook.com>
2022-10-31 02:59:56 +0900

    Add goerli network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.HARDHAT_ALCHEMY_API_URL,
+      accounts: [process.env.HARDHAT_PRIVATE_KEY || '']
+    }
+  }
 };
 
 export default config;
