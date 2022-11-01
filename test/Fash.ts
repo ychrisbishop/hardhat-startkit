9b8d7058931f06814b420270c6237e41ab56fce9
Xander Q <xanderqiang@gmail.com>
2022-11-01 16:56:36 +0100

    test1

--- test/Fash.ts
+++ test/Fash.ts
@@ -1,12 +1,15 @@
 import { ethers } from 'hardhat'
 
-describe('flash loan', function () {
+describe('flash loan test', function () {
+
+    let FlashTest
+    let flashTest
 
     async function deploy () {
-
+        FlashTest = await ethers.getContractFactory("UniswapV3FlashTest")
     }
-    describe('deploy', function () {
-        it('test flash loan function', async () => {
+    describe('deploy and test flash loan sample function', function () {
+        it('contract address', async () => {
             const FlashTest = await ethers.getContractFactory("UniswapV3FlashTest")
             const flashTest = await FlashTest.deploy()
 
@@ -14,8 +17,9 @@ describe('flash loan', function () {
 
             console.log('flash test contract address: ', flashTest.address)
 
-            const fee = await flashTest.testFlash()
-            console.log('fee is : ', fee)
+            const value = await flashTest.testFlash()
+            console.log('fee is : ', value)
         })
     })
+    
 })
