63b0352aa51472963d736df6fa3e17854616ae9d
Xander Q <xanderqiang@outlook.com>
2022-11-10 12:45:13 +0900

    Flash loan test

--- contracts/UniswapV3FlashTest.sol
+++ contracts/UniswapV3FlashTest.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// import "forge-std/Test.sol";
-// import "forge-std/console.sol";
-
 import "hardhat/console.sol";
 import "./UniswapV3Flash.sol";
 
 contract UniswapV3FlashTest {
+
     // mainnet
     // address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     // address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     //testnet
-    address constant WETH = 0x0Bb7509324cE409F7bbC4b701f932eAca9736AB7;
-    address constant USDC = 0xd35CCeEAD182dcee0F148EbaC9447DA2c4D449c4;
+    address constant WETH = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    address constant USDC = 0x07865c6E87B9F70255377e024ace6630C1Eaa37F;
 
     uint24 constant POOL_FEE = 3000;
+
+    uint amount = 0;
 
     IWETH private weth = IWETH(WETH);
     IERC20 private usdc = IERC20(USDC);
@@ -24,30 +24,32 @@ contract UniswapV3FlashTest {
     UniswapV3Flash private uni = new UniswapV3Flash(USDC, WETH, POOL_FEE);
 
     event Flash(uint, uint);
+    event Deposit(uint);
 
     function setUp() public {}
 
-    function testFlash() public pure returns (uint) {
-        // Approve WETH fee
-        // uint256 amount = 1e16;
-        // weth.deposit{value: amount}();
-        // weth.approve(address(this), amount);
-        // uint256 allowance = weth.allowance(msg.sender, address(this));
+    function test () public pure returns (uint) {
+        uint a = 256;
+
+        return a;
+    }
+
+    function deposit () external payable {
+        amount = msg.value;
+        weth.deposit{value: amount}();
+        weth.approve(address(uni), amount);
+    }
+
+    function testFlash() public returns (uint256) {
+
+        uint balBefore = weth.balanceOf(address(this));
+        uni.flash(0, 100 * amount);
+        uint balAfter = weth.balanceOf(address(this));
+
+        uint fee = balBefore - balAfter;
+
+        emit Flash(fee, amount);
         
-        // console.log(allowance);
-        // weth.transferFrom(msg.sender, address(this), amount);
-        // weth.approve(address(uni), amount);
-
-        // uint balBefore = weth.balanceOf(address(this));
-        // uni.flash(0, 100 * amount);
-        // uint balAfter = weth.balanceOf(address(this));
-
-        // uint fee = balBefore - balAfter;
-        // console.log("WETH fee", fee);
-        uint fee = 100;
-        // emit Flash(allowance, fee);
-        // return allowance;
         return fee;
-        // return (allowance, fee);
     }
 }
--- test/Fash.ts
+++ test/Fash.ts
@@ -1,24 +1,74 @@
 import { ethers } from 'hardhat'
+import { expect, assert, should, util } from 'chai'
+import { BigNumber } from 'ethers'
 
-describe('flash loan test', function () {
+describe('FLASH LOAN TEST', function () {
 
-    let FlashTest
-    let flashTest
+    let FlashTest: any
+    let flashTestContract: any
+
+    const ONE_GWEI = 1_000_000_000;
 
     async function deploy () {
         FlashTest = await ethers.getContractFactory("UniswapV3FlashTest")
+        flashTestContract = await FlashTest.deploy()
+
+        flashTestContract.deployed()
     }
-    describe('deploy and test flash loan sample function', function () {
-        it('contract address', async () => {
-            const FlashTest = await ethers.getContractFactory("UniswapV3FlashTest")
-            const flashTest = await FlashTest.deploy()
+    const log = (...args:unknown[])=>{
+        setTimeout(()=>{
+            let indent = "\t";
+            args = args.map(a=>util.inspect(a, true, 100, true).replace(/^\x1B\[32m['"`]/ig, '\x1B[32m').replace(/['"`]\x1B\[39m$/ig, '\x1B[39m').replace(/\n/g, `\n${indent} `))
+            // args = args.map(a=>util.inspect(a))
+            console.log.call(console, indent, ...args);
+        })
+    }
+    beforeEach(async () => {
+        // await deploy()
+    })
+    // util
+    describe('deploy and test flash loan sample function', async function () {
+        it('test feature', async () => {
+            // ethers.utils.
+            const amount = BigNumber.from('1')
+            console.log(amount)
+            // log(amount)
+        })
+        xit('contract address', async () => {
+            
+            const testValue = await flashTestContract.test()
 
-            flashTest.deployed()
+            log('test value', testValue)
 
-            console.log('flash test contract address: ', flashTest.address)
+            const filter = {
+                address: flashTestContract,
+                topics: [
+                    ethers.utils.id("Flash(uint, uint)")
+                ]
+            }
+            const abi = ["event Flash(uint, uint)"]
+            // flashTestContract.on(filter, (allowance: any, fee: any, event: any) => {
+            //     log('< flash event >', allowance, fee, event)
+            // })
+            // flashTestContract.on('Flash', (allowance: any, fee: any, event: any) => {
+            //     log('< flash event >', allowance, fee, event)
+            // })
+            let iface = new ethers.utils.Interface(abi)
 
-            const value = await flashTest.testFlash()
-            console.log('fee is : ', value)
+            log(flashTestContract.address)
+            const amount = BigNumber.from(2e15);
+            log('send weth', amount)
+
+            const tx = await flashTestContract.testFlash({ value: amount })
+            log(tx)
+            const logs = (await tx.wait()).logs
+            // log(logs[0])
+            logs.map((item: any) => {
+                iface.parseLog(item).args.map((arg: any) => {
+                    log(arg)
+                })
+            })
+
         })
     })
     
