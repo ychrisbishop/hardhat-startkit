b5dd7aad10ad3d54c201b4d5242bd3a04bce2559
chris bishop <ychrisbishop@gmail.com>
2022-12-08 10:44:49 +0100

    Create TriangularSwapper.sol

---
+++ contracts/TriangularSwapper.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import './BaseSwapperV2.sol';
+
+contract TriangularSwapper is BaseSwapperV2 {
+  function execute(
+      address _borrowAsset,
+      uint256 _borrowAmount,
+      address _repayAsset,
+      uint256 _repayAmount,
+      bytes memory _executionData
+  ) public override {
+    
+  }
+}
