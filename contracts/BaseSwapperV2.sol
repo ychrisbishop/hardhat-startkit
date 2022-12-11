649b7fbd2137b68dec1de9df3dce99c740057122
chris bishop <ychrisbishop@gmail.com>
2022-12-11 03:35:04 +0100

    Add solmate module

--- contracts/BaseSwapperV2.sol
+++ contracts/BaseSwapperV2.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {IUniswapV2Pair} from "./interfaces/IUniswapV2Pair.sol";
-import {ERC20, SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+import {ERC20} from "./lib/solmate/src/tokens/ERC20.sol";
+import { SafeTransferLib } from "./lib/solmate/src/utils/SafeTransferLib.sol";
 import {UnsafeUnilib} from "./lib/UnsafeUnilib.sol";
 
 // We are throwing away requires that are mostly hitting us if we have bad input.
